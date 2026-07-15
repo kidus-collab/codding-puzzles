f56bc9c2006a7bdcabd0c8825c287ba7ad595496
kidus-collab <65975552+kidus-collab@users.noreply.github.com>
2026-07-15 11:07:51 -0400

    Feat: Day 3

--- advent-of-code/2026/filesystem.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-declare(strict_types=1); 
-//file system 
-$battries = readfile('problem.txt');
-
-echo $battries; 
---
+++ advent-of-code/2026/filesystem/filesystem.php
@@ -0,0 +1,9 @@
+<?php 
+
+declare(strict_types=1); 
+//file system 
+//$battries = readfile('problem.txt');
+
+// mkdir('foo/r' , recursive: true);
+
+unlink('foot.txt');
